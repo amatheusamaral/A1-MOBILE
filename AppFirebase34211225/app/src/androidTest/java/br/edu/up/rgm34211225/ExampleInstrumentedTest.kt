a77d8fd0b016f872db154642f1b044b4d94d56f8
amatheusamaral <164792874+amatheusamaral@users.noreply.github.com>
2024-11-26 20:19:54 -0300

    Contexto do aplicativo em teste

--- AppFirebase34211225/app/src/androidTest/java/br/edu/up/rgm34211225/ExampleInstrumentedTest.kt
+++ AppFirebase34211225/app/src/androidTest/java/br/edu/up/rgm34211225/ExampleInstrumentedTest.kt
@@ -12,12 +12,12 @@ import org.junit.Assert.*
  * Instrumented test, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
- */
+ */ 
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
-        // Context of the app under test.
+       // Contexto do aplicativo em teste
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("br.edu.up.rgm34211225", appContext.packageName)
     }
